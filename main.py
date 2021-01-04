3ed9547e9285b13d78c25819c540d1264b4d9dba
as595 <anna.scaife@manchester.ac.uk>
2021-01-04 14:37:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ normalise= transforms.Normalize((config_dict['data']['datamean'],), (config_dict
 transform = transforms.Compose([
     crop,
     pad,
-#    transforms.RandomRotation(360, resample=Image.BILINEAR, expand=False),
+    transforms.RandomRotation(360, resample=Image.BILINEAR, expand=False),
     totensor,
     normalise,
 ])

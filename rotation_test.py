5c98ec525e697408f6413c0b797c96abeadc8111
as595 <anna.scaife@manchester.ac.uk>
2021-01-07 16:45:15 +0000

    Add files via upload

--- rotation_test.py
+++ rotation_test.py
@@ -59,7 +59,7 @@ transform = transforms.Compose([
 ])
 
 
-test_data = locals()[config_dict['data']['dataset']](config_dict['data']['datadir'], train=False, transform=transform)
+test_data = locals()[config_dict['data']['dataset']](config_dict['data']['datadir'], train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 N = len(test_loader)
